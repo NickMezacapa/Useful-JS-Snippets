a491bbc5fa795f5fb5261a8853498514877c177a
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-07-05 13:07:42 -0700

    commit to master

--- Insertion Sort/index.js
+++ Insertion Sort/index.js
@@ -13,7 +13,7 @@ Pseudocode:
 const insertionSort = arr => {
     for (var i = 1; i < arr.length; i++) {
         var currentVal = arr[i];
-        for (var j = i -1; j >= 0 && j > currentVal; j++) {
+        for (var j = i -1; j >= 0 && j > currentVal; j--) {
             arr[j+1] = arr[j];
         }
         arr[j+1] = currentVal;
